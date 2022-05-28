177ace6e940933226f2ae6b2709cb16fe2ecc424
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-05-28 02:13:37 -0400

    add portfolio pieces

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,11 +1,33 @@
-export default function ProjectCard() {
+import styles from "./ProjectCard.module.css";
+
+export default function ProjectCard(props) {
+	const { title, liveLink, githubRepo, image } = props;
 	return (
-		<div className="max-w-lg min-w-300px w-full rounded-md overflow-hidden shadow-lg">
-			<img
-				className="w-full"
-				src="https://v1.tailwindcss.com/img/card-top.jpg"
-				alt="Sunset in the mountains"
-			/>
-		</div>
+		<>
+			<div className={styles.card}>
+				<img src={image} alt="Sunset in the mountains" />
+				<div className={styles.info}>
+					<h5>{title}</h5>
+					<div className="flex flex-row gap-2 justify-center items-center">
+						<a
+							className="text-white bg-pink-500 hover:bg-pink-600 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-fit"
+							href={liveLink}
+							target="_blank"
+							rel="noreferrer"
+						>
+							Live
+						</a>
+						<a
+							className="text-white bg-pink-500 hover:bg-pink-600 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-fit"
+							href={githubRepo}
+							target="_blank"
+							rel="noreferrer"
+						>
+							Code
+						</a>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 }
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,19 +1,60 @@
 import styles from "./Work.module.css";
 import ProjectCard from "../ProjectCard";
+import project1 from "./project1.png";
+import project2 from "./project2.png";
+import project3 from "./project3.png";
+
 export default function Work() {
+	const projects = [
+		{
+			id: 1,
+			title: "Work In Progress",
+			liveLink: "",
+			githubRepo: "",
+			image: project1,
+		},
+		{
+			id: 2,
+			title: "Simple Blog v2",
+			liveLink: "https://simple-blog-v2.vercel.app/",
+			githubRepo: "https://github.com/uajanth/simple-blog-v2",
+			image: project2,
+		},
+		{
+			id: 3,
+			title: "Magic Eden Calculator",
+			liveLink: "https://magicedencalculator.vercel.app/",
+			githubRepo: "https://github.com/uajanth/magicedencalculator",
+			image: project3,
+		},
+	];
 	return (
 		<>
-			<header id="work" className={styles.header}>
-				<h3>My Work</h3>
-				<h1 className="font-sans mb-10 text-5xl p-0.5 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500">
-					Some of my latest projects.
-				</h1>
-				<div className="flex flex-col xl:flex-row gap-20 xl:gap-10 items-center justify-center">
-					<ProjectCard />
-					<ProjectCard />
-					<ProjectCard />
-				</div>
-			</header>
+			<section
+				id="work"
+				className="w-full flex flex-col items-center justify-center mb-10"
+			>
+				<header className={styles.header}>
+					<h3>My Work</h3>
+					<h1 className="font-sans text-5xl p-0.5 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500">
+						Some of my latest projects.
+					</h1>
+				</header>
+
+				<main className={styles.container}>
+					<div className="flex flex-col xl:flex-row gap-20 xl:gap-10 items-center justify-center">
+						{projects.map((project) => (
+							<ProjectCard
+								key={project.id}
+								title={project.title}
+								image={project.image}
+								liveLink={project.liveLink}
+								githubRepo={project.githubRepo}
+							/>
+						))}
+					</div>
+				</main>
+			</section>
 		</>
 	);
 }
