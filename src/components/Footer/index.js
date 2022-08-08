91b05e07fda4eae13a8b96941aa3ecd1047449dc
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-08-08 17:19:25 -0400

    add portrait photo and move socials to the header

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,22 +5,6 @@ function Footer() {
 	const currentYear = new Date().getFullYear();
 	return (
 		<footer className={styles["footer"]}>
-			<div className={styles["links-container"]}>
-				<a
-					href="https://www.github.com/uajanth"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<ion-icon name="logo-github"></ion-icon>
-				</a>
-				<a
-					href="https://www.linkedin.com/in/ajanthuthayan"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<ion-icon name="logo-linkedin"></ion-icon>
-				</a>
-			</div>
 			<small>Created by Ajanth Uthayan</small>
 			<small className={styles["copyright"]}>
 				&copy; Copyright {currentYear}. All Rights Reserved.
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,36 @@
 import styles from "./Header.module.css";
+import portrait from "./portrait.png";
 
 export default function Header() {
 	return (
-		<>
-			<header className={styles.header}>
-				<h1 className="font-sans text-7xl xs:text-8xl sm:text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-400">
+		<header className={styles.header}>
+			<div className={styles["col1"]}>
+				<img src={portrait} alt="Ajanth Uthayan" />
+			</div>
+			<div className={styles["col2"]}>
+				<h1 className="font-sans text-7xl xs:text-8xl sm:text-9xl lg:text-9xl xl:text-10xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-400">
 					Ajanth Uthayan
 				</h1>
 				<h2 className="font-sans text-2xl font-bold color-white">
 					Software Developer
 				</h2>
-				{/* <button
-					type="button"
-					className=" cursor-not-allowed text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 mb-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700"
-					disabled
-				>
-					View Resume
-				</button> */}
-			</header>
-		</>
+				<div className={styles["links-container"]}>
+					<a
+						href="https://www.github.com/uajanth"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<ion-icon name="logo-github"></ion-icon>
+					</a>
+					<a
+						href="https://www.linkedin.com/in/ajanthuthayan"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<ion-icon name="logo-linkedin"></ion-icon>
+					</a>
+				</div>
+			</div>
+		</header>
 	);
 }
